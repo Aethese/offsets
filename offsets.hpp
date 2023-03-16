c130048cfbd931ad4fd82eddc572121b6f9af8e4
Aethese <59108437+Aethese@users.noreply.github.com>
2023-03-16 14:58:28 -0500

    add offsets

---
+++ offsets.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace offsets
+{
+	uint64_t dwEntityList   = 0x4DFFEF4;
+	uint64_t dwForceAttack  = 0x322DCFC;
+	uint64_t dwLocalPlayer  = 0xDEA964;
+	uint64_t m_fFlags       = 0x104;
+	uint64_t m_iCrosshairId = 0x11838;
+	uint64_t m_iTeamNum     = 0xF4;
+}
