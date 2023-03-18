2bcf2e1cdf678d0bd2bfc6a0a838889fd3f39979
Aethese <59108437+Aethese@users.noreply.github.com>
2023-03-18 14:00:19 -0500

    change type

--- offsets.hpp
+++ offsets.hpp
@@ -2,10 +2,10 @@
 
 namespace offsets
 {
-	uint64_t dwEntityList   = 0x4DFFEF4;
-	uint64_t dwForceAttack  = 0x322DCFC;
-	uint64_t dwLocalPlayer  = 0xDEA964;
-	uint64_t m_fFlags       = 0x104;
-	uint64_t m_iCrosshairId = 0x11838;
-	uint64_t m_iTeamNum     = 0xF4;
+	uintptr_t dwEntityList   = 0x4DFFEF4;
+	uintptr_t dwForceAttack  = 0x322DCFC;
+	uintptr_t dwLocalPlayer  = 0xDEA964;
+	uintptr_t m_fFlags       = 0x104;
+	uintptr_t m_iCrosshairId = 0x11838;
+	uintptr_t m_iTeamNum     = 0xF4;
 }
