3af25e88efba7509e6b537cd2732c58bcf26faae
Aethese <59108437+Aethese@users.noreply.github.com>
2023-03-29 08:08:00 -0500

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -2,9 +2,9 @@
 
 namespace offsets
 {
-	uintptr_t dwEntityList   = 0x4DFFEF4;
-	uintptr_t dwForceAttack  = 0x322DCFC;
-	uintptr_t dwLocalPlayer  = 0xDEA964;
+	uintptr_t dwEntityList   = 0x4E01024;
+	uintptr_t dwForceAttack  = 0x322EE5C;
+	uintptr_t dwLocalPlayer  = 0xDEB964;
 	uintptr_t m_fFlags       = 0x104;
 	uintptr_t m_iCrosshairId = 0x11838;
 	uintptr_t m_iTeamNum     = 0xF4;
