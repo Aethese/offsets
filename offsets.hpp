05f8dac1ca9e3ccd10fbcf42fd23f6fa9b725d5b
Aethese <59108437+Aethese@users.noreply.github.com>
2023-05-06 07:29:18 -0500

    Update offsets.hpp
    dwEntityList, dwForceAttack, dwLocalPlayer

--- offsets.hpp
+++ offsets.hpp
@@ -2,9 +2,9 @@
 
 namespace offsets
 {
-	uintptr_t dwEntityList   = 0x4DFFFB4;
-	uintptr_t dwForceAttack  = 0x322DDEC;
-	uintptr_t dwLocalPlayer  = 0xDEA964;
+	uintptr_t dwEntityList   = 0x4DFEECC;
+	uintptr_t dwForceAttack  = 0x322CD38;
+	uintptr_t dwLocalPlayer  = 0xDE997C;
 	uintptr_t m_fFlags       = 0x104;
 	uintptr_t m_iCrosshairId = 0x11838;
 	uintptr_t m_iTeamNum     = 0xF4;
